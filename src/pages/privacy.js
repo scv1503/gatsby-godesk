aa02f70f154d72b9b0df4676f345b5eb2a406dde
Salomon <106621232+scv1503@users.noreply.github.com>
2022-09-09 08:30:55 -0700

    Update privacy.js

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -4,9 +4,9 @@ import { LegalSection, Page, Seo } from "gatsby-theme-portfolio-minimal";
 export default function PrivacyPage() {
   return (
     <>
-      <Seo title="Privacy Policy" useTitleTemplate={true} noIndex={true} />
+      <Seo title="Politicas de Privacidad" useTitleTemplate={true} noIndex={true} />
       <Page>
-        <LegalSection sectionId="privacy" heading="Privacy Policy" />
+        <LegalSection sectionId="privacy" heading="Politicas de Privacidad" />
       </Page>
     </>
   );
